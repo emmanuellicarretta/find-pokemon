400fddc9c92a1cd362566092940409bd62828c44
manucarretta <emmanuellicarretta@gmail.com>
2022-05-22 20:56:39 -0300

    input and button search pokemon at Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Grid, TextField, Typography } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Home: React.FC = () => {
 
@@ -15,15 +16,33 @@ const Home: React.FC = () => {
         alignItems="center"
         sx={{ p: 5 }}
       >
-        <Grid item textAlign="center" xs={12} sx={{ pt: 2, pb: 2 }}>
+
+        <Grid item textAlign="center" xs={12} sx={{ pt: 2 }}>
           <Typography variant="h1">Vamos caçar pokemons?</Typography>
         </Grid>
-        <Grid item textAlign="center" xs={6} sx={{ pb: 4 }}>
-          <TextField fullWidth label="Pesquisar" id="fullWidth" />
+
+        <Grid container
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          sx={{ pb: 4, pt: 4 }}
+        >
+          <Grid item xs={8}>
+            <TextField fullWidth label="Digite aqui o nome do pokemon ou o ID dele e pesquise" id="fullWidth" />
+          </Grid>
+          <Grid item textAlign="left" xs={1}>
+            <Button>
+              <SearchIcon fontSize="large" style={{ color: "black" }} />
+            </Button>
+          </Grid>
+
         </Grid>
 
+
       </Grid>
-    </div>
+    </div >
   );
 };
 
